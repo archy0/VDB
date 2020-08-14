72ed3f5f341cc3c17ba67d08eb4e2a7a0313861a
Artūrs Kļaviņš <hazyak02@gmail.com>
2020-08-14 10:01:15 +0300

    HEAD labojums

---
+++ VDB_0.9.3-master/DTOs/Art_Dok_SaiteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VDB.DTOs {
+    public class Art_Dok_SaiteDTO {
+        public decimal ID { get; set; }
+        public decimal DOK_ID { get; set; }
+        public decimal VIETA_CD { get; set; }
+        public DateTime? DAT_MOD { get; set; }
+        public Arg_AdreseDTO Arg_Adrese { get; set; }
+        public Art_DokumentsDTO Art_Dokuments { get; set; }
+    }
+}
